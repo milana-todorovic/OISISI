052eb8747fdc6630aad055fde48151491223a0e3
milana-todorovic <milanatodorovic@uns.ac.rs>
2019-11-24 13:45:00 +0100

    #inicijalizacija_repozitorijuma

---
+++ StudentskaSluzba/src/main/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package main;
+
+/**
+ * @author Milana Todorovic ra3-2017
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
