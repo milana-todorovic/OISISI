22a55fc0950da919c95cadcbb5db9914b2d46279
Ana Perisic <anaperisictb@yahoo.com>
2020-01-05 14:28:44 +0100

    #dodavanje_studenta Izgled prozora za dodavanje i validacija polja

--- StudentskaSluzba/src/view/validity_utils/Validator.java
+++ StudentskaSluzba/src/view/validity_utils/Validator.java
@@ -12,6 +12,7 @@ import java.util.regex.PatternSyntaxException;
  * formatima.
  * 
  * @author Milana Todorovic ra3-2017
+ * @author Ana Perisic ra1-2017
  *
  */
 public class Validator {
@@ -19,13 +20,61 @@ public class Validator {
 	public static Boolean isAlphanumeric(String s) {
 		return s.matches("[\\p{IsAlphabetic}0-9]*");
 	}
-	
+
 	public static Boolean isAlphanumericWithSeparators(String s) {
 		return s.matches("[\\p{IsAlphabetic}0-9 -]*");
 	}
 
+	public static Boolean isAlphanumericWithSeparator(String s) {
+		return s.matches("[\\p{IsAlphabetic}0-9-]*");
+	}
+
+	public static Boolean isAlphanumericWithDot(String s) {
+		return s.matches("[\\p{IsAlphabetic}0-9.@]*");
+	}
+
+	public static Boolean isNumeric(String s) {
+		return s.matches("[0-9]*");
+	}
+
+	public static Boolean isNumericWithComma(String s) {
+		return s.matches("[0-9,]*");
+	}
+
+	public static Boolean isNumericWithSep(String s) {
+		return s.matches("[0-9-]*");
+	}
+
+	public static Boolean isAlphabeticWithSeparators(String s) {
+		return s.matches("[\\p{IsAlphabetic} -]*");
+	}
+
 	public static Boolean isNazivPredmeta(String s) {
 		return s.matches("[\\p{IsAlphabetic}0-9]+([ ]*-?[ ]*[\\p{IsAlphabetic}0-9]+)*");
+	}
+
+	public static Boolean isImePrezimeStudenta(String s) {
+		return s.matches("[\\p{IsAlphabetic}]+([ ]*-?[ ]*[\\p{IsAlphabetic}]+)*");
+	}
+
+	public static Boolean isAdresaStudenta(String s) {
+		return s.matches("([\\p{IsAlphabetic}]+[ ]*)+[0-9]+[a-zA-Z]*(-[0-9]+)*");
+	}
+
+	public static Boolean isEmailAdresa(String s) {
+		return s.matches("[\\p{IsAlphabetic}]+.?[\\p{IsAlphabetic}]+[0-9]*@([a-z]+.)+[a-z]+");
+	}
+
+	public static Boolean isBrojIndeksa(String s) {
+		return s.matches("(([\\p{IsAlphabetic}][0-9])|([\\p{IsAlphabetic}][a-zA-Z]))[0-9]{1,3}-[2][0][0-9]{2}");
+	}
+
+	public static Boolean isBrojTel(String s) {
+		return s.matches("[0-9]{9,20}");
+	}
+
+	public static Boolean isProsek(String s) {
+		return s.matches("([6-9],[0-9][0-9])|([1][0],[0][0])");
 	}
 
 	public static Boolean matchesSearchFormat(String s) {
